1b6b2b04a326a071665df40cd6e8748231ffb308
Wilson Vargas <wilson.vargas@processimlabs.com>
2018-05-22 17:49:14 -0500

    first commit

---
+++ src/DemoAppCSS/DemoAppCSS/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoAppCSS.ViewModels
+{
+    public class MainPageViewModel
+    {
+        public double Result { get; set; } = 0;
+
+    }
+}
---
+++ src/DemoAppCSS/DemoAppCSS/Views/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoAppCSS.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DemoAppCSS.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MaingPage : ContentPage
+    {
+        public MaingPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainPageViewModel();
+        }
+    }
+}
